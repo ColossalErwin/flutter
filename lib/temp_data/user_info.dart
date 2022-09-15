a7caa5ccca02a52e62f67bb44c28de57b108d488
ColossalErwin <83517339+ColossalErwin@users.noreply.github.com>
2022-09-14 20:41:47 -0500

    Update user_info.dart

--- lib/temp_data/user_info.dart
+++ lib/temp_data/user_info.dart
@@ -1,13 +1,10 @@
-//user_info (temporary store user_info in the app, so that we don't need to reload again)
-//remember to update these in case we update username and userimage
-
 bool hasLoadedCredential = false;
 String? userImageURL;
 String? username;
 String? userEmail;
 
 void reset() {
-  hasLoadedCredential = false;
+  hasLoadedCredential = false; //has fetched username, userImageURL, and user email yet?
   userEmail = null;
   userImageURL = null;
   userEmail = null;
